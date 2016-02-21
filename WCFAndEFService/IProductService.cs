7a4aea54e3baa96d7443bedd9b7c5df2ab4a1499
sachinpande <sachpan77@gmail.com>
2016-02-20 21:39:39 -0500

    First Check in

---
+++ WCFAndEFService/IProductService.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WCFAndEFService
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        Products GetProductID(int d);
+    }
+
+    [DataContract]
+    public class Products
+    {
+        [DataMember]
+        public int ProductID { get; set; }
+
+        [DataMember]
+        public string ProductName { get; set; }
+
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+
+        [DataMember]
+        public decimal UnitPrice { get; set; }
+
+        [DataMember]
+        public bool Discontinued { get; set; }
+
+    }
+}
---
+++ WCFAndEFService/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFAndEFService
+{
+    public class ProductService : IProductService
+    {
+        public Products GetProductID(int id)
+        {
+            //var product = new Product()
+            //{
+            //    ProductID = id,
+            //    ProductName = "IDK",
+            //    UnitPrice = (decimal)10.0,
+            //};            
+            //return product;
+            Products product = null;
+            using (var context = new NorthwindEntities1())
+            {
+                var productEntity = (from p in context.Products
+                                     where p.ProductID == id
+                                     select p).FirstOrDefault();
+                if(productEntity != null)
+                {
+                    product = TranslateProductEntityToProduct(productEntity);
+                }
+                else
+                {
+                    throw new Exception(string.Format("Invalid productID {0}", id));
+                }
+            }
+            return product;
+        }
+
+        private Products TranslateProductEntityToProduct(Product productEntity)
+        {
+            Products p = new Products()
+            {
+                Discontinued = productEntity.Discontinued,
+                ProductID = productEntity.ProductID,
+                ProductName = productEntity.ProductName,
+                QuantityPerUnit = productEntity.QuantityPerUnit,
+                UnitPrice = (decimal)productEntity.UnitPrice
+            };
+            return p;
+        }
+    }
+}
