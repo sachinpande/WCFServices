dd7189ab957cb6263441c9ebe9b9b03c05812c6a
sachinpande <sachpan77@gmail.com>
2016-03-10 12:33:14 -0500

    TCP Hosting added

---
+++ ConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFAndEFService;
+
+namespace ConsoleHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ProductService));
+            host.Open();
+            Console.WriteLine("Service started. Press [Enter] to exit.");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
