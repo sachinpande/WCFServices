3543aa7c9ca3fa9cf15751462f2e913d8752c4c4
sachinpande <sachpan77@gmail.com>
2016-03-27 19:26:27 -0400

    Config file less

--- ConsoleHost2/Program.cs
+++ ConsoleHost2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using WCFAndEFService;
@@ -13,6 +14,10 @@ namespace ConsoleHost2
         static void Main(string[] args)
         {
             ServiceHost host = new ServiceHost(typeof(ProductService));
+            string address = "net.tcp://localhost:8009/ProductService";
+            Binding binding = new NetTcpBinding();
+            Type contract = typeof(IProductService);
+            host.AddServiceEndpoint(contract, binding, address);
             host.Open();
             Console.WriteLine("Service started. Press [Enter] to exit.");
             Console.ReadLine();
